7c15d047cbe126eb73300cff6f290a9e6b3c4647
Suphatchara-Wongprawich <68465404+Suphatchara-Wongprawich@users.noreply.github.com>
2020-08-10 23:13:19 +0700

    Course-06.2-6-Step 5: Add the click handler for the Clear button

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -95,6 +95,18 @@ class SleepTrackerViewModel(
             database.update(night)
         }
     }
+    fun onClear() {
+        uiScope.launch {
+            clear()
+            tonight.value = null
+        }
+    }
+
+    suspend fun clear() {
+        withContext(Dispatchers.IO) {
+            database.clear()
+        }
+    }
 
 }
 
